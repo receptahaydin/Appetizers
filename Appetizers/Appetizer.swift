3dac5f9a4f96bdacf56b7cb269e5f02362077889
recepaydin <receptaha.aydin@stu.fsm.edu.tr>
2024-05-16 11:51:00 +0300

    Appetizer model generated and MockData created for testing

---
+++ Appetizers/Appetizer.swift
@@ -0,0 +1,34 @@
+//
+//  Appetizer.swift
+//  Appetizers
+//
+//  Created by Recep Taha Aydın on 16.05.2024.
+//
+
+import Foundation
+
+struct Appetizer: Decodable {
+    let id: Int
+    let name: String
+    let description: String
+    let price: Double
+    let imageURL: String
+    let calories: Int
+    let protein: Int
+    let carbs: Int
+}
+
+struct AppetizerResponse {
+    let request: [Appetizer]
+}
+
+struct MockData {
+    static let sampleAppetizer = Appetizer(id: 0001,
+                                           name: "Test Appetizer",
+                                           description: "This is the description for my appetizer. It's yummy.",
+                                           price: 9.99,
+                                           imageURL: "",
+                                           calories: 99,
+                                           protein: 99,
+                                           carbs: 99)
+}
