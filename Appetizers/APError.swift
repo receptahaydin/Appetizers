4dc71446107945e449ab546185dbd8db0a58ab96
recepaydin <receptaha.aydin@stu.fsm.edu.tr>
2024-05-16 13:23:50 +0300

    APError created and GetAppetizer func implemented

---
+++ Appetizers/APError.swift
@@ -0,0 +1,15 @@
+//
+//  APError.swift
+//  Appetizers
+//
+//  Created by Recep Taha Aydın on 16.05.2024.
+//
+
+import Foundation
+
+enum APError: Error {
+    case invalidURL
+    case invalidResponse
+    case invalidData
+    case unableToComplete
+}
