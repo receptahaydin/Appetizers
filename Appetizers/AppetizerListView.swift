96dff267be0f6eca589f7dcf44a034dc475942f9
recepaydin <receptaha.aydin@stu.fsm.edu.tr>
2024-05-16 13:34:02 +0300

    Appetizers showed on ListView via Network call inside DispatchQueue

--- Appetizers/AppetizerListView.swift
+++ Appetizers/AppetizerListView.swift
@@ -8,13 +8,32 @@
 import SwiftUI
 
 struct AppetizerListView: View {
+    
+    @State private var appetizers: [Appetizer] = []
+    
     var body: some View {
         NavigationView {
-            List(MockData.appetizers) { appetizer in
+            List(appetizers) { appetizer in
                 AppetizerListCell(appetizer: appetizer)
             }
             .listStyle(.plain)
             .navigationTitle("🍟 Appetizers")
+        }
+        .onAppear {
+            getAppetizers()
+        }
+    }
+    
+    func getAppetizers() {
+        NetworkManager.shared.getAppetizers { result in
+            DispatchQueue.main.async {
+                switch result {
+                case.success(let appetizers):
+                    self.appetizers = appetizers
+                case.failure(let error):
+                    print(error.localizedDescription)
+                }
+            }
         }
     }
 }
