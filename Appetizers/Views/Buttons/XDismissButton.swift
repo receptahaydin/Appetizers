50c89bb3d633f411cfb055c24ff4d3c7f681d92b
recepaydin <receptaha.aydin@stu.fsm.edu.tr>
2024-05-18 23:32:46 +0300

    Made some refactors like ViewModel, APButton, XDismissButton

---
+++ Appetizers/Views/Buttons/XDismissButton.swift
@@ -0,0 +1,28 @@
+//
+//  XDismissButton.swift
+//  Appetizers
+//
+//  Created by Recep Taha Aydın on 18.05.2024.
+//
+
+import SwiftUI
+
+struct XDismissButton: View {
+    var body: some View {
+        ZStack {
+            Circle()
+                .frame(width: 30, height: 30)
+                .foregroundColor(.white)
+                .opacity(0.6)
+            
+            Image(systemName: "xmark")
+                .imageScale(.small)
+                .frame(width: 44, height: 44)
+                .foregroundColor(.black)
+        }
+    }
+}
+
+#Preview {
+    XDismissButton()
+}
