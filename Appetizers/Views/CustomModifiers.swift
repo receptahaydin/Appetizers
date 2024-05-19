25e24ba044f36559613d8dc347cf803febccb39c
recepaydin <receptaha.aydin@stu.fsm.edu.tr>
2024-05-20 00:36:54 +0300

    For the standard button style of iOS, added CustomModifiers, badge added to TabBar for counting order items numbers

---
+++ Appetizers/Views/CustomModifiers.swift
@@ -0,0 +1,24 @@
+//
+//  CustomModifiers.swift
+//  Appetizers
+//
+//  Created by Recep Taha Aydın on 20.05.2024.
+//
+
+import SwiftUI
+
+struct StandardButtonStyle: ViewModifier {
+    
+    func body(content: Content) -> some View {
+        content
+            .buttonStyle(.bordered)
+            .tint(.brandPrimary)
+            .controlSize(.large)
+    }
+}
+
+extension View {
+    func standardButtonStyle() -> some View {
+        self.modifier(StandardButtonStyle())
+    }
+}
