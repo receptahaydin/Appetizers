61cc663c6e9019d5cfebe7c898739e98c0b867dd
recepaydin <receptaha.aydin@stu.fsm.edu.tr>
2024-05-19 09:33:51 +0300

    Order list mock data created for testing, also OrderView ui part started and delete process completed

--- Appetizers/Screens/OrderView/OrderView.swift
+++ Appetizers/Screens/OrderView/OrderView.swift
@@ -8,10 +8,30 @@
 import SwiftUI
 
 struct OrderView: View {
+    
+    @State private var orderItems = MockData.orderItems
+    
     var body: some View {
         NavigationView {
-            Text("Orders")
-                .navigationTitle("🧾 Orders")
+            VStack {
+                List {
+                    ForEach(orderItems) { appetizer in
+                        AppetizerListCell(appetizer: appetizer)
+                    }
+                    .onDelete(perform: { indexSet in
+                        orderItems.remove(atOffsets: indexSet)
+                    })
+                }
+                .listStyle(.plain)
+                
+                Button {
+                    print("order placed")
+                } label: {
+                    APButton(title: "$99.99 - Place Order")
+                }
+                .padding(.bottom, 25)
+            }
+            .navigationTitle("🧾 Orders")
         }
     }
 }
