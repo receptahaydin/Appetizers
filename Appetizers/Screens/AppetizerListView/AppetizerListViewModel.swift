0ba7ca31ebf0a10215b53ac3589f6056194d0fcf
recepaydin <receptaha.aydin@stu.fsm.edu.tr>
2024-05-20 20:24:13 +0300

    Error case handled with switch case, for the images used AsyncImage, guard let syntax changes applied which comes with iOS 16 and regex changed

--- Appetizers/Screens/AppetizerListView/AppetizerListViewModel.swift
+++ Appetizers/Screens/AppetizerListView/AppetizerListViewModel.swift
@@ -51,7 +51,21 @@ import SwiftUI
                 appetizers = try await NetworkManager.shared.getAppetizers()
                 isLoading = false
             } catch {
-                alertItem = AlertContext.generic
+                if let apError = error as? APError {
+                    switch apError {
+                    case .invalidURL:
+                        alertItem = AlertContext.invalidURL
+                    case .invalidResponse:
+                        alertItem = AlertContext.invalidResponse
+                    case .invalidData:
+                        alertItem = AlertContext.invalidData
+                    case .unableToComplete:
+                        alertItem = AlertContext.unableToComplete
+                    }
+                } else {
+                    alertItem = AlertContext.generic
+                }
+                
                 isLoading = false
             }
         }
